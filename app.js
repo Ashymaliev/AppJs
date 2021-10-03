a657e794e719f7a54eaaff80ad705b32db796c5c
Ashymaliev <Nursultan-99kg@mail.ru>
2021-10-03 21:25:52 +0600

    scroll eventlistener

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const navLinks =document.querySelectorAll(".nav__link");
 console.log(navLinks);
 
 function sayHello() {
-    let message ="Hello Micky Mouse";
-    console.log(message);
+    let message ="Hello Micky Mouse " + MyName;
+    console.log(message); 
 }
  
 sayHello();
+
+function simpleMath(a, b) {
+     let result = a + b;
+     return result;
+}
+
+let sum =  simpleMath(11, 382);
+
+console.log(sum);
+
+
+window.addEventListener("scroll", function(){
+    console.log("scrolled")
+});
