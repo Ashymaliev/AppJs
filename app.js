cac4edec68b2e3c84364d88601c630c939e3cfaf
Ashymaliev <Nursultan-99kg@mail.ru>
2021-10-03 18:43:17 +0600

    abt selectors

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('test')
+const MyName ="Nursultan";
+let age = 22; 
+
+const header =document.querySelector("#header");
+const navLinks =document.querySelectorAll(".nav__link");
+
+console.log( navLinks)
